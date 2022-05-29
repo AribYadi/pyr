b69c49b8fdf036978c2916c86ccfa3bf0f73c7d3
AribYadi <aribyadi@gmail.com>
2022-05-29 08:06:32 +0700

    feat(tester): improve tester

--- test.py
+++ test.py
@@ -1,107 +1,146 @@
 #!/usr/bin/env python3
 
-import glob
+from dataclasses import dataclass
 from os import path
 import os
 import subprocess
-import pathlib
 import argparse
 import sys
+from typing import BinaryIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--update", help = "Update the tests outputs", action = "store_true")
+parser.add_argument("-r", "--release", help = "Run tests in release mode", action = "store_true")
 parser.add_argument("-d", "--dir", help = "Directory to run tests in", default = "tests")
 args = parser.parse_args()
 
 BASE_DIR = path.dirname(__file__)
-os.chdir(BASE_DIR)
 
-PYR_BINARY = path.join(BASE_DIR, "target", "debug", "pyr")
+PYR_RELEASE_BINARY = path.join(BASE_DIR, "target", "debug", "pyr")
+PYR_DEBUG_BINARY = path.join(BASE_DIR, "target", "debug", "pyr")
 TEST_DIR = path.join(BASE_DIR, args.dir)
 
 PYR_EXT = ".pyr"
 RECORD_EXT = ".record"
 
-EXIT_RUN_PASS = 0
-EXIT_RUN_FAIL = 1
+END_TAG = "\n:end:\n"
 
-passing = True
-n_fail = 0
-n_pass = 0 
+@dataclass
+class TestCase():
+  exitcode: int
+  stdout: bytes
+  stderr: bytes
 
-def fail():
-  global passing
-  passing = True
-  n_fail += 1
+  def write(self, path: str) -> bool:
+    try:
+      if TestCase.read(path) == self:
+        return False
+    except (FileNotFoundError, AssertionError):
+      pass
+    with open(path, "wb") as f:
+      TestCase.write_int_field(f, "exitcode", self.exitcode)
+      TestCase.write_blob_field(f, "stdout", self.stdout)
+      TestCase.write_blob_field(f, "stderr", self.stderr)
+      return True
+
+  @classmethod
+  def read(cls, path: str) -> "TestCase":
+    with open(path, "rb") as f:
+      exitcode = TestCase.read_int_field(f, "exitcode")
+      stdout = TestCase.read_blob_field(f, "stdout")
+      stderr = TestCase.read_blob_field(f, "stderr")
+    return cls(exitcode, stdout, stderr)
+
+  @staticmethod
+  def write_blob_field(f: BinaryIO, name: str, blob: bytes):
+    TestCase.write_int_field(f, name, len(blob))
+    f.write(blob)
+    f.write(END_TAG.encode())
+
+  @staticmethod
+  def write_int_field(f: BinaryIO, name: str, value: int):
+    f.write(f":{name} {value}:\n".encode())
+  
+  @staticmethod
+  def read_blob_field(f: BinaryIO, name: str) -> bytes:
+    line = f.readline()
+    field = f":{name} ".encode()
+    assert line.startswith(field), f"Broken record file. Missing blob `{name}` tag."
+    assert line.endswith(b":\n"), f"Broken record file. Missing closing tag of blob `{name}`."
+    size = int(line[len(field):-2])
+    blob = f.read(size)
+    assert f.read(len(END_TAG)) == END_TAG.encode(), f"Broken record file. Missing end of blob `{name}` tag."
+    return blob
+  
+  @staticmethod
+  def read_int_field(f: BinaryIO, name: str) -> int:
+    line = f.readline()
+    field = f":{name} ".encode()
+    assert line.startswith(field), f"Broken record file. Missing int `{name}` tag."
+    assert line.endswith(b":\n"), f"Broken record file. Missing closing tag of int `{name}`."
+    return int(line[len(field):-2])
+
+@dataclass
+class TestResults():
+  passed: int = 0
+  failed: int = 0
+  skipped: int = 0
+  updated: int = 0
 
 def relative_path(path):
   return str(path).replace(BASE_DIR, ".")
 
-def test_file(path):
-  output = subprocess.run([PYR_BINARY, path], capture_output = True)
-  output_file = pathlib.Path(path).with_suffix(RECORD_EXT)
+def test_file(path, results: TestResults):
+  output = subprocess.run([PYR_DEBUG_BINARY, path], capture_output = True)
+  test_case = TestCase(output.returncode, output.stdout, output.stderr)
 
-  def decode(stream):
-    stream = stream.decode()
-    output = stream.replace(BASE_DIR, "$DD")
-    return output
-
-  exitcode = output.returncode
-  stdout = decode(output.stdout)
-  stderr = decode(output.stderr)
+  tc_path = path[:-len(PYR_EXT)] + RECORD_EXT
+  tc_path = relative_path(tc_path)
   
-  file_path_relative = relative_path(output_file)
   if args.update:
-    with open(output_file, "w") as f:
-      f.write(f":code {exitcode}:\n")
-      f.write(f":stdout {len(stdout)}:\n")
-      f.write(stdout)
-      f.write("\n")
-      f.write(f":stderr {len(stderr)}:\n")
-      f.write(stderr)
-      f.write("\n")
-      f.write(f":end")
-      print(f"\x1b[2;96m[INFO]\x1b[0m: {file_path_relative} updated.")
+    print(f"\x1b[2;96m[INFO]\x1b[0m: Updating `{tc_path}`..")
+    if test_case.write(tc_path):
+      print(f"\x1b[2;96m[INFO]\x1b[0m: {tc_path} updated.")
+      results.updated += 1
+    else:
+      print(f"\x1b[2;96m[INFO]\x1b[0m: {tc_path} is up to date. Skipping.")
+      results.skipped += 1
+  elif not os.path.exists(tc_path):
+    print(f"\x1b[33m[WARN]\x1b[0m: Couldn't find record file for {tc_path}. Skipping.")
+    results.skipped += 1
   else:
-    with open(output_file, "r") as f:
-      expected = f.read();
+    print(f"\x1b[2;96m[INFO]\x1b[0m: Testing `{tc_path}`..")
+    expected_test_case = TestCase.read(tc_path)
 
-      assert expected.__contains__(f":code "), "Broken record file. Missing `code` tag."
-      assert expected.__contains__(f":stdout "), "Broken record file. Missing `stdout` tag."
-      assert expected.__contains__(f":stderr "), "Broken record file. Missing `stderr` tag."
-      assert expected.__contains__(f":end"), "Broken record file. Missing `end` tag."
+    if test_case == expected_test_case:
+      print(f"\x1b[2;96m[INFO]\x1b[0m: {tc_path} passed.")
+      results.passed += 1
+    else:
+      print(f"\x1b[1;31m[ERR]\x1b[0m: {tc_path} failed.", file = sys.stderr)
+      print(f"\x1b[1;31m[ERR]\x1b[0m: Expected:", file = sys.stderr)
+      print(f"  exit code: {expected_test_case.exitcode}", file = sys.stderr)
+      print(f"  stdout: {expected_test_case.stdout}", file = sys.stderr)
+      print(f"  stderr: {expected_test_case.stderr}", file = sys.stderr)
+      print(f"\x1b[1;31m[ERR]\x1b[0m: Got:", file = sys.stderr)
+      print(f"  exit code: {test_case.exitcode}", file = sys.stderr)
+      print(f"  stdout: {test_case.stdout}", file = sys.stderr)
+      print(f"  stderr: {test_case.stderr}", file = sys.stderr)
+      results.failed += 1
 
-      expected_exitcode = int(expected.split(":code ")[1].split(":", 1)[0])
-      expected_stdout_len = int(expected.split(":stdout ")[1].split(":", 1)[0])
-      expected_stdout = expected.split(":stdout ")[1].split(":\n", 1)[1].split("\n:stderr ", 1)[0]
-      expected_stderr_len = int(expected.split(":stderr ")[1].split(":", 1)[0])
-      expected_stderr = expected.split(":stderr ")[1].split(":\n", 1)[1].split("\n:end", 1)[0]
+if __name__ == "__main__":
+  os.chdir(BASE_DIR)
+  results = TestResults()
+  for entry in os.scandir(TEST_DIR):
+    if entry.is_file() and entry.path.endswith(PYR_EXT):
+      test_file(entry.path, results)
 
-      if exitcode == expected_exitcode and len(stdout) == expected_stdout_len and stdout == expected_stdout and len(stderr) == expected_stderr_len and stderr == expected_stderr:
-        global n_pass
-        print(f"\x1b[2;96m[INFO]\x1b[0m: {file_path_relative} passed.")
-        n_pass += 1
-      else:
-        global n_fail
-        print(f"\x1b[1;31m[ERR]\x1b[0m: {file_path_relative} failed.", file = sys.stderr)
-        print(f"\x1b[1;31m[ERR]\x1b[0m: Expected:", file = sys.stderr)
-        print(f"  exit code: {expected_exitcode}", file = sys.stderr)
-        print(f"  stdout: \n{expected_stdout}", file = sys.stderr)
-        print(f"  stderr: \n{expected_stderr}", file = sys.stderr)
-        print(f"\x1b[1;31m[ERR]\x1b[0m: Got:", file = sys.stderr)
-        print(f"  exit code: {exitcode}", file = sys.stderr)
-        print(f"  stdout: \n{stdout}", file = sys.stderr)
-        print(f"  stderr: \n{stderr}", file = sys.stderr)
-        n_fail += 1
-
-for entry in os.scandir(TEST_DIR):
-  if entry.is_file() and entry.path.endswith(PYR_EXT):
-    test_file(entry.path)
-
-print()
-print(f"\x1b[2;96m[INFO]\x1b[0m: {n_pass + n_fail} tested.")
-print(f"\x1b[2;96m[INFO]\x1b[0m: {n_pass} passed.")
-print(f"\x1b[2;96m[INFO]\x1b[0m: {n_fail} failed.")
-
-if not passing:
-  exit(1)
+  if args.update:
+    print()
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.updated} updated.")
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.skipped} skipped.")
+  else:
+    print()
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.passed + results.failed} tested.")
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.passed} passed.")
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.failed} failed.")
+    print(f"\x1b[2;96m[INFO]\x1b[0m: {results.skipped} skipped.")
