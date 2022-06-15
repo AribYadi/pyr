0b10bbd0cc2d3a7e3a1ca84878f2bff8fd88ec4d
AribYadi <aribyadi@gmail.com>
2022-06-15 13:56:00 +0700

    feat: add additional testing tools

---
+++ run_tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./test.py -d examples -D --always-build
+./test.py -d tests -D --always-build
---
+++ update_tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./test.py -d examples -u -D --always-build
+./test.py -d tests -u -D --always-build
