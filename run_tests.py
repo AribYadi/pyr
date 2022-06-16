53da09d2256337c624d606767f5948ca1f62f8df
AribYadi <aribyadi@gmail.com>
2022-06-16 09:03:37 +0700

    fix(tester): exit with errcode 1 if any test fails

--- run_tests.py
+++ run_tests.py
@@ -3,5 +3,9 @@
 import subprocess
 
 if __name__ == "__main__":
-  subprocess.run(["python3", "./test.py", "-d", "examples", "-D", "--always-build"])
-  subprocess.run(["python3", "./test.py", "-d", "tests", "-D", "--always-build"])
+  output = subprocess.run(["python3", "./test.py", "-d", "examples", "-D", "--always-build"])
+  if output.returncode != 0:
+    exit(1)
+  output = subprocess.run(["python3", "./test.py", "-d", "tests", "-D", "--always-build"])
+  if output.returncode != 0:
+    exit(1)
