30e9f9e8f40aa2eb9a0cb482d5bbbc5bf234910e
AribYadi <aribyadi@gmail.com>
2022-06-28 12:27:32 +0700

    feat(tester): run `cargo test` as well

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+from re import sub
 import subprocess
 
 if __name__ == "__main__":
+  output = subprocess.run(["cargo", "test"])
+  if output.returncode != 0:
+    exit(1)
   output = subprocess.run(["python3", "./test.py", "-d", "examples", "-D", "--always-build"])
   if output.returncode != 0:
     exit(1)
