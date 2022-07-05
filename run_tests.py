70f696a0368c65a801182f87bacd0514b92ddce4
AribYadi <aribyadi@gmail.com>
2022-07-05 08:05:35 +0700

    fix(tester): use `ntest` than `test`

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ from re import sub
 import subprocess
 
 if __name__ == "__main__":
-  output = subprocess.run(["cargo", "test"])
+  output = subprocess.run(["cargo", "ntest"])
   if output.returncode != 0:
     exit(1)
   output = subprocess.run(["python3", "./test.py", "-d", "examples", "-D", "--always-build"])
